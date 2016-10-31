8f096e8cd8d9ea1a8fddb2ae6742f59f3ebf8715
“leilei2016 <1572431041@qq.com>
2016-10-31 21:41:47 +0800

    update

--- src/main/java/com/sselab/springboot/book/Application.java
+++ src/main/java/com/sselab/springboot/book/Application.java
@@ -1,10 +1,25 @@
 package com.sselab.springboot.book;
 
+import javax.sql.DataSource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
 
 @SpringBootApplication(
 		scanBasePackages = "com.sselab.springboot.book"
@@ -12,7 +27,7 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 	
 	private static Log log = LogFactory.getLog(Application.class);
-
+	
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
